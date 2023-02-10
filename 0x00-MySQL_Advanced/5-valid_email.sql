14928a3bfc90ba222c322c50143747d28179f2fc
Daniel Okyere <daniel.kwame.okyere@gmail.com>
2023-02-10 19:25:59 +0000

    Done 5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- Script that creates a trigger that resets te attribute
+DELIMETER $$;
+CREATE TRIGGER validate BEFORE UPDATE ON users
+FOR EACH ROW
+	BEGIN
+		IF NEW.email != OLD.email THEN
+			SET NEW.valid_email = 0;
+		END IF;
+END;$$
+delimeter;
