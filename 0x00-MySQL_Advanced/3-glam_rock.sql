1eb14cc56a7adbc8c0c73ab6fd0a484a4a24aa0c
Daniel Okyere <daniel.kwame.okyere@gmail.com>
2023-02-10 18:52:07 +0000

    3-glam done

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,4 @@
--- SQL script select band_name and lifespan
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY lifespan DESC;
+-- SQL script that lists all bands
+
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM
+metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
